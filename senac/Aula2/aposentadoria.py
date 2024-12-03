dc99671949c13b8cf06972fb1d01e0a9ea830bfa
RyujinXz <matheuspluralldelas@gmail.com>
2024-12-02 21:42:48 -0300

    12-02

--- senac/Aula2/aposentadoria.py
+++ senac/Aula2/aposentadoria.py
@@ -5,5 +5,8 @@ if (idade >= 60) or (contribuicao >= 35):
 else:
     aposentarI = 60-idade
     aposentarC = 35-contribuicao
+print("-"*30)
+print("RESULTADO")
+print("-"*30)
 print(f"Por Idade: {aposentarI} anos restantes")
 print(f"Por Contrubuição: {aposentarC} anos restantes")
