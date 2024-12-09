a6c873f8a52af6624af24cc0093e3897f0b15899
RyujinXz <matheuspluralldelas@gmail.com>
2024-12-08 21:36:02 -0300

    home

---
+++ Aula4/exercicioClasse/dvr9.py
@@ -0,0 +1,6 @@
+class Funcionario:
+    def __init__(self, nome: str, horasTrabalhadas: int, valorHora: float):
+        self.nome = nome
+        self.horasTrabalho = horasTrabalhadas 
+        self.valorHora = valorHora 
+    
